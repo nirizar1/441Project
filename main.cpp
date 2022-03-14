6efcf976bb0fc55044bb1287b07a8ed9268c3831
nirizar1 <101524490+nirizar1@users.noreply.github.com>
2022-03-13 20:08:26 -0400

    Added Quick Sort tests and visualization.
    You need to have csvs in the same directory as the python notebook for the visualizations to work. I'm going to work on space visualizations next.

--- main.cpp
+++ main.cpp
@@ -56,8 +56,8 @@ void write_csv(std::string filename, std::vector<std::pair<std::string, std::vec
 
 
 
-bool test(){
-    string fileName  = "results.csv"; // target csv where outputs will be logged
+bool test(string file){
+    string fileName  = file; // target csv where outputs will be logged
 
     if (FILE *file = fopen(fileName.c_str(), "r")) {
         fclose(file);
@@ -68,7 +68,7 @@ bool test(){
     } 
 }
 
-void logFile(bool exists, int arraySize, double clockTicks, double seconds){
+void logFile(string file, bool exists, int arraySize, double clockTicks, double seconds){
 
     vector<int> size;
     vector<double> ticks;
@@ -82,12 +82,12 @@ void logFile(bool exists, int arraySize, double clockTicks, double seconds){
     secondsVector.push_back(seconds);
 
     if(!exists){
-        ofstream myFile("results.csv");
+        ofstream myFile(file);
         //cout << "File creation successful" << endl;
     }
         
     std::vector<std::pair<std::string, std::vector<double>>> vals = {{"", size_double}, {"", ticks}, {"", secondsVector}};
-    write_csv("results.csv", vals);
+    write_csv(file, vals);
     //cout << "Write successful" << endl;
 }
 
@@ -103,8 +103,51 @@ void insertionSort(int array[], int arrSize){
     }
 }
 
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
-void test_cases(int size){
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high]; // pivot
+    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+ 
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void test_cases_insertion_sort(int size){
 
     int arrSize = size;      //size of array
     int randArray[arrSize]; //creating empty array
@@ -123,22 +166,55 @@ void test_cases(int size){
     stop = clock();
     T = stop - start;           //number of clock ticks the algorithm took
 
+    bool exists = test("insertion_sort_results.csv");                 //check if the output file exists
+
+    logFile("insertion_sort_results.csv",exists, arrSize, T, T/CLOCKS_PER_SEC); //writing to the log file
+}
+
+void test_cases_quick_sort(int size){
+    int arrSize = size;
+    int startIndex = 0;
+    int endIndex = arrSize - 1;
+
+    int randArray[arrSize]; //creating empty array
+
+    for(int i = 0; i < arrSize; i++){           //assigning random values in array
+        randArray[i] = rand()% arrSize;
+    }
+
+    clock_t start, stop;    //stores the clock ticks while running the program
+    double T = 0.0;         // number of total ticks
+
+    start = clock();        //clock start
+
+    quickSort(randArray,startIndex, endIndex);  //algorithm function callS
+
+    stop = clock();
+    T = stop - start;           //number of clock ticks the algorithm took
+
     //cout << "Sorting " << arrSize << " array elements in random array" << " took " << T<< " clock ticks ("<< T/CLOCKS_PER_SEC << " seconds)!" << endl;
 
-    bool exists = test();                 //check if the output file exists
+    bool exists = test("quick_sort_results.csv");                 //check if the output file exists
 
-    logFile(exists, arrSize, T, T/CLOCKS_PER_SEC); //writing to the log file
+    logFile("quick_sort_results.csv",exists, arrSize, T, T/CLOCKS_PER_SEC); //writing to the log file
 }
 
 int main(){
 
     srand (time(NULL)); //random seed
 
+    cout << "Loading test cases for insertion sort. Please wait." << endl;
     for(int i = 0; i < 1000; i++){
-        int randomArrSize = rand() % 100000 + 4000;
-        test_cases(randomArrSize);
+        int randomArrSize = rand() % 10000 + 3000;
+        test_cases_insertion_sort(randomArrSize);
     }
-
-    cout << "Program end." << endl;
+    cout << "Insertion sort tests completed." << endl;
+    cout << endl;
+    cout << "Loading test cases for quick sort. Please wait." << endl;
+    for(int i = 0; i < 1000; i++){
+        int randomArrSize = rand() % 10000 + 3000;
+        test_cases_quick_sort(randomArrSize);
+    }
+    cout << "Quick Sort tests completed \n Program end."<< endl;
     return 0;
 }
