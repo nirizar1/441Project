11fe18848b966361442e95534f66c0ac82ca38ea
dkrivera1 <drivera1@umbc.edu>
2022-05-10 19:15:17 -0400

    Added base quicksort

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+#include <bits/stdc++.h>
+ 
+// A utility function to swap two elements
+void swap(float* a, float* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
 // Utility function to swap elements `A[i]` and `A[j]` in an array
 void partition(float a[], int l, int r, int& i, int& j)
@@ -75,4 +84,40 @@ void threeWayQuicksort(float a[], int l, int r)
     // Recur
     threeWayQuicksort(a, l, j);
     threeWayQuicksort(a, i, r);
+}
+int partition (float arr[], int low, int high)
+{
+    float pivot = arr[high]; // pivot
+    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+ 
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+ 
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(float arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
