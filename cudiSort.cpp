be166d8c4e5e5939b116d43aa85adc17db1ec63b
dkrivera1 <drivera1@umbc.edu>
2022-05-10 20:27:07 -0400

    Cudi Sort finalized

--- cudiSort.cpp
+++ cudiSort.cpp
@@ -5,78 +5,44 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-/*
-int FULCRUM = 100;
 
-void cudiSort(float arr[], int n){
-  // 1) Create n empty buckets
-  vector<float> b[n];
-  
-  // 2) Put array elements
-  // in different buckets
-  for (int i = 0; i < n; i++) {
-    int bi = n * arr[i]; // Index in bucket
-    b[bi].push_back(arr[i]);
-  }
-  
-  // 3) Sort individual buckets
-  for (int i = 0; i < n; i++)
-  {
-    float *bucket = &b[i][0];
-    int bucket_size = b[i].size();
-    if(bucket_size <= FULCRUM)
-    {
-        insertionSort(bucket, bucket_size);
-    }
-    else
-    {
-        threeWayQuicksort(bucket, 0, bucket_size);
-    }
-
-  }
-  // 4) Concatenate all buckets into arr[]
-  int index = 0;
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < int(b[i].size()); j++)
-      arr[index++] = b[i][j];
-}
-*/
+int FULCRUM_BS = 100;
+int FULCRUM_QS = 100;
 
 void cudiSort(float arr[], int n){
     //int score = scoreData(arr,size);
 
 
-    //insertion sort for small inputs 
-    if(n < 100){
+    // Insertion sort for small inputs 
+    if(n < FULCRUM_BS){
         insertionSort(arr, n);
     }
-    //threeway partition for intermidate size and high sort
-    else{
-      threeWayQuicksort(arr, 0, n-1);
-      /*
-        vector<float> b[n];
-  
-  // 2) Put array elements
-  // in different buckets
-  for (int i = 0; i < n; i++) {
-    int bi = n * arr[i]; // Index in bucket
-    b[bi].push_back(arr[i]);
-  }
-  
-  // 3) Sort individual buckets
-  for (int i = 0; i < n; i++)
-  {
-    float *bucket = &b[i][0];
-    int bucket_size = b[i].size();
-    threeWayQuicksort(bucket, 0, bucket_size - 1);
+    // Modified Bucket Sort for large inputs
+    else
+    {
+      vector<float> b[n];
+      for (int i = 0; i < n; i++) {
+        int bi = n * arr[i];
+        b[bi].push_back(arr[i]);
+      }
 
-  }
-  // 4) Concatenate all buckets into arr[]
-  int index = 0;
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < int(b[i].size()); j++)
-      arr[index++] = b[i][j];
-      */
+      for (int i = 0; i < n; i++)
+      {
+        // Insertion Sort for small buckets
+        if(b[i].size() < FULCRUM_QS)
+        {
+          insertionSort(&b[i][0], b[i].size());
+        }
+        // 3Way Quick Sort for large buckets
+        else
+        {
+          quickSort(&b[i][0], 0, b[i].size() - 1);
+        }
+      }
+    int index = 0;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < int(b[i].size()); j++)
+        arr[index++] = b[i][j];
     }
 }
 
